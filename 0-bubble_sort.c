6753dc40d2b645fa87764e565a25a15f2f5122a1
angel bermudez <angelo1998bg@gmail.com>
2024-04-08 11:53:27 -0400

    bubble sort now compiles

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -4,21 +4,17 @@
 *@array: array to be sorted
 *@size: full size of array
 */
-void bubble_sort(int *array, size_t size)
-{
-int *temp;
-int size = size_t(size n)
+void bubble_sort(int *array, size_t size) {
+    int temp;
 
-for (int i = 0; i < size; i++)
-{
-    for (int j = 0; j < size - i - 1; j++)
-        if (array[j] > array[j + 1])
-        {
-            temp = array[j];
-            array[j] = array[j + 1];
-            array[j + 1] = temp;
+    for (size_t i = 0; i < size; i++) {
+        for (size_t j = 0; j < size - i - 1; j++) {
+            if (array[j] > array[j + 1]) {
+                temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+            }
         }
-
-print_array(array, size);
-}
+	print_array(array, size);
+    }
 }
