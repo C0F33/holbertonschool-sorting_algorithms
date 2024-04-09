0accbed5280021d898be8fe55c5bea92e01f3fc1
angel bermudez <angelo1998bg@gmail.com>
2024-04-09 15:55:01 -0400

    somechanges to bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,20 +6,25 @@
 */
 void bubble_sort(int *array, size_t size)
 {
-    int temp;
-size_t i, j;
+    int temp, swapped;
+    size_t i, j;
 
     for (i = 0; i < size; i++)
-	{
+    {
+        swapped = 0;
         for (j = 0; j < size - i - 1; j++)
-		{
+        {
             if (array[j] > array[j + 1])
-			{
+            {
                 temp = array[j];
                 array[j] = array[j + 1];
                 array[j + 1] = temp;
+                swapped = 1;
             }
         }
-	print_array(array, size);
+        if (swapped == 0)
+            break;
+
+        print_array(array, size);
     }
 }
