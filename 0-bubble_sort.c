0bc3c3cbcfa61b1e171b7302b2619e328cb2922d
angel bermudez <angelo1998bg@gmail.com>
2024-04-09 16:13:35 -0400

    it works

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,36 +1,32 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sorts an array of integers in ascending order using Bubble sort
+ * bubble_sort - sorts an array of integers in ascending order using Bubble
  * @array: array to be sorted
  * @size: size of the array
  */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j;
-    int tmp;
-    int swapped;
+	size_t i, j;
+	int tmp;
+	int swapped;
 
-    for (i = 0; i < size - 1; i++)
-    {
-        swapped = 0;
-        for (j = 0; j < size - i - 1; j++)
-        {
-            if (array[j] > array[j + 1])
-            {
+	for (i = 0; i < size - 1; i++)
+	{
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+				swapped = 1;
+				print_array(array, size);
+			}
+		}
 
-                tmp = array[j];
-                array[j] = array[j + 1];
-                array[j + 1] = tmp;
-                swapped = 1;
-
-
-                print_array(array, size);
-            }
-        }
-
-
-        if (swapped == 0)
-            break;
-    }
+		if (swapped == 0)
+			break;
+	}
 }
