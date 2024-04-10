b755787000f64bbc884049c5f3fa65edd0c2ad72
angel bermudez <angelo1998bg@gmail.com>
2024-04-10 10:44:04 -0400

    fixed mixed declaration2

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -8,12 +8,12 @@
 */
 void selection_sort(int *array, size_t size)
 {
-	if (array == NULL || size < 2)
-		return;
-
 	size_t i;
 	size_t j;
 	int tmp;
+	if (array == NULL || size < 2)
+		return;
+
 
 	for (i = 0; i < size - 1; i++)
 	{
